62633b22317675ab4a67829f9d98dc7d8b5da5d9
Kelsea Fields <kelsea.fields@yahoo.com>
2021-01-13 19:15:57 -0800

    models

---
+++ Develop/models/workoutModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: String,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Select workout type",
+      },
+      name: {
+        type: String,
+        required: "Select workout name",
+      },
+      duration: {
+        type: Number,
+        required: "Add total duration length",
+      },
+      weight: {
+        type: Number,
+        required: "weight used",
+      },
+      reps: {
+        type: Number,
+        required: "How many reps",
+      },
+      sets: {
+        type: Number,
+        required: "how many sets",
+      },
+      distance: {
+        type: Number,
+        required: "length of distance",
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
