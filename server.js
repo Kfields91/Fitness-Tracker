9a7e270c93aecb728a17574621729cd51fd224fa
Kelsea Fields <kelsea.fields@yahoo.com>
2021-01-16 15:39:56 -0800

    deploying

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // Handle API routes HERE
