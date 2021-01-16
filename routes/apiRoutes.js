cdfd854cdb9c13d7538b13fbc8b75481a636613a
Kelsea Fields <kelsea.fields@yahoo.com>
2021-01-16 15:19:31 -0800

    finishing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,13 @@ var Workout = require("../models/workoutModel");
 
 router.get("/api/workouts", (req, res) => {
   console.log("I am a GET route for /workout");
-  Workout.find({})
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: ["$exercises.duration"] },
+      },
+    },
+  ])
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -57,12 +63,19 @@ router.get("/api/workouts/range", function (req, res) {
   Workout.aggregate([
     {
       $addFields: {
-        totalDuration: { $sum: exercise.duration },
+        totalDuration: { $sum: ["$exercises.duration"] },
       },
     },
-  ]).then(function (dbWorkout) {
-    res.json(dbWorkout);
-  });
+  ])
+    .sort({ day: -1 })
+    .limit(7)
+    .sort({ day: 1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
